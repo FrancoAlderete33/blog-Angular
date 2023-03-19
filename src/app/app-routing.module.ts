7380190519f64b175f5ec96c096ab91e62336996
FrancoAlderete33 <fran.alderete77@gmail.com>
2023-03-19 16:51:35 +0000

    inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { Page404Component } from './page404/page404.component';
+import { PostOneComponent } from './post-one/post-one.component';
+import { PostThreeComponent } from './post-three/post-three.component';
+import { PostTwoComponent } from './post-two/post-two.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'post-1', component: PostOneComponent },
+  { path: 'post-2', component: PostTwoComponent },
+  { path: 'post-3', component: PostThreeComponent },
+  { path: '**', component: Page404Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { PostOneComponent } from './post-one/post-one.component';
+import { PostTwoComponent } from './post-two/post-two.component';
+import { PostThreeComponent } from './post-three/post-three.component';
+import { Page404Component } from './page404/page404.component';
+import { CarruselComponent } from './carrusel/carrusel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
+    PostOneComponent,
+    PostTwoComponent,
+    PostThreeComponent,
+    Page404Component,
+    CarruselComponent
   ],
   imports: [
     BrowserModule,
