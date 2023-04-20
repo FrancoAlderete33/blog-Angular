fffb91d6c68e72d32f9d08cc9a53084d1932a3fc
FrancoAlderete33 <118802908+FrancoAlderete33@users.noreply.github.com>
2023-04-20 19:03:20 +0200

    cambios en articulo 1 - sql

--- src/app/post-one/post-one.component.ts
+++ src/app/post-one/post-one.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class PostOneComponent {
 
+  secciones = [
+    {name : '¿Qué es SQL y para qué se utiliza?', id: 'seccion-1'},
+    {name : '¿Qué son las bases de datos?', id: 'seccion-2'},
+    {name : 'Estructura de una base de datos', id: 'seccion-3'},
+    {name : 'Tipos de datos en SQL', id: 'seccion-4'},
+    {name : 'Consultas básicas en SQL', id: 'seccion-5'},
+    {name : 'Funciones y operadores en SQL', id: 'seccion-6'},
+    {name : 'Ordenamiento y agrupamiento de datos en SQL', id: 'seccion-7'},
+    {name : 'Modificación de datos en SQL', id: 'seccion-8'},
+    {name : 'Unión de tablas en SQL', id: 'seccion-9'},
+  ];
+
+  constructor(){}
+
+
+
 }
