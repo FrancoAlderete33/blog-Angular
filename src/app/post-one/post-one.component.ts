c92d4039e14572d7fa57092361c6379cc55ddf79
FrancoAlderete33 <118802908+FrancoAlderete33@users.noreply.github.com>
2023-04-27 22:46:55 +0200

    button TOP

--- src/app/post-one/post-one.component.ts
+++ src/app/post-one/post-one.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-post-one',
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-one.component.css']
 })
 export class PostOneComponent {
+
+  showButton: boolean = false;
 
   secciones = [
     {name : '¿Qué es SQL y para qué se utiliza?', id: 'seccion-1'},
@@ -20,6 +22,30 @@ export class PostOneComponent {
   ];
 
   constructor(){}
+
+  ngOnInit(): void {}
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: any) {
+    const scrollPosition = window.pageYOffset;
+    const windowSize = window.innerHeight;
+    const bodyHeight = document.body.offsetHeight;
+    const maxScroll = bodyHeight - windowSize - 300; // Agrega un margen de 100px
+  
+    if (scrollPosition >= 3000 && scrollPosition < maxScroll) {
+      this.showButton = true;
+    } else {
+      this.showButton = false;
+    }
+  }
+  
+
+  scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  }
 
 
 
